d8b13dc988abf3fed8f8b06f4ced345fe58f5918
pu <857484057@qq.com>
2017-12-20 13:50:56 +0800

    ....

--- src/pcMain/StringUtils.java
+++ src/pcMain/StringUtils.java
@@ -10,45 +10,6 @@ public class StringUtils {
         return  (string.startsWith(Parameter.ONLINE_SUCCESS) && string.endsWith(Parameter.END_FLAG));
     }
 
-    public static boolean isBind(String s){
-        return s.startsWith(Parameter.CONNECTED_SUCCESS)&&s.endsWith(Parameter.END_FLAG);
-    }
-
-    /**
-     *  读取流中的字符
-     * @param reader
-     * @return
-     */
-
-    public static String readLine(BufferedReader reader) {
-        StringBuilder sb = new StringBuilder();
-        String temp = "";
-        try {
-            while (!(temp = reader.readLine()).endsWith(Parameter.END_FLAG)) {
-                sb.append(temp);
-            }
-            sb.append(temp);
-        } catch (IOException e) {
-            System.out.println("读取数据失败。。。");
-            e.printStackTrace();
-        }
-        return sb.toString();
-    }
-
-    /**
-     * 字符串结尾加 end_flag结尾标志
-     * @param s
-     * @return
-     */
-    public static String addEnd_flag2Str(String s){
-        return s+"_"+Parameter.END_FLAG;
-    }
-
-/*public static String rmEnd_flagstr(String s){
-    int endflagindex=s.lastIndexOf("_");
-    return s.substring(0,endflagindex);
-}*/
-
     public static Content getContent(String s){
         int head=s.indexOf("_");
         int tail=s.lastIndexOf("_");
